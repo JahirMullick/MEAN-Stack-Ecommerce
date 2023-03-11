9b9e5b8aa7b26913800a93347b3d0ed77fa0de58
Jahir <jahirmj2001@gmail.com>
2023-03-11 11:23:08 +0530

    Add token and cookie

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -2,15 +2,15 @@
 
 const sendToken = (user, statusCode, res) => {
     const token = user.getJWTToken();
-
+    const days = parseInt(process.env.COOKIE_EXPIRE.match(/^\d+/)[0]);
+    const expires = new Date(Date.now() + days * 24 * 60 * 60 * 1000);
     // options for cookie
     const options = {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-        ),
+        expires,
         httpOnly: true,
     };
-
+    // console.log('expires:', expires);
+    // console.log('options:', options);
     res.status(statusCode).cookie("token", token, options).json({
         success: true,
         user,
